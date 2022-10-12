54986f6c55934ddfbcc89004140d418f5e12cbd1
rheneadianne <rheneadianne@gmail.com>
2022-10-12 07:17:17 -0400

    heroku launched

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,11 +13,8 @@ butInstall.addEventListener('click', async () => {
     if (!promptEvent) {
      return;
     }
-  
     promptEvent.prompt();
-    
     window.deferredPrompt = null;
-    
     butInstall.classList.toggle('hidden', true);
 });
 
