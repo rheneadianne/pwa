c59a4edeaeeba4e742255c5491a11921a3a398e0
rheneadianne <rheneadianne@gmail.com>
2022-10-12 06:49:53 -0400

    // TODO: Add and configure workbox plugins for a service worker and manifest file.// TODO: Add CSS loaders and babel to webpack.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE Text Editor',
+      }),
+      new WebpackPwaManifest({
+          name: 'JATE Text Editor',
+          short_name: 'JATE',
+          description: 'Edit Text!',
+          background_color: '#7eb4e2',
+          theme_color: '#7eb4e2',
+          start_url: './',
+          publicPath: './',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            }
+          ],
+        }), 
+        new InjectManifest({
+          swSrc: './sw.js',
+          swDest: 'src-sw.js',
+        }), 
+      ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            },
+          },
+        },
       ],
     },
   };
